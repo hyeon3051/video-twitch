3ce44e17bcebbe48a0dccc5ab5e321e00b4325cb
hyeon3051 <hyeon3051@gmail.com>
2025-01-11 20:15:23 +0900

    Add Webpack fallback for crypto-browserify in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["83eeq32jm8.ufs.sh"],
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        crypto: require.resolve("crypto-browserify"),
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
